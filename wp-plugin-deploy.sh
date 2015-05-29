e0c3bc26e289a8d52b468b9b9a468f8a28d8197d
Peter Harlacher <pm@helvetian.io>
2015-05-29 10:39:44 +0700

    Adjusted script name

--- wp-plugin-deploy.sh
+++ wp-plugin-deploy.sh
@@ -108,7 +108,7 @@ echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific files and deployment script"
-svn propset svn:ignore "deploy.sh
+svn propset svn:ignore "wp-plugin-deploy.sh
 README.md
 .git
 .gitignore" "$SVNPATH/trunk/"
